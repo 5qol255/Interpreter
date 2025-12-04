12568ac0544927680b1da4d80b1286e1a5c2dcb0
5qol255 <149298356+5qol255@users.noreply.github.com>
2025-12-04 13:57:20 +0800

    fix(main): update main function to accept command line arguments for input and output files

--- FDLI.cpp
+++ FDLI.cpp
@@ -1,14 +1,21 @@
-#include "FDLI/scanner.hpp"
 #include "FDLI/parser.hpp"
 #include "FDLI/drawer.hpp"
 
-int main()
+int main(int argc, char *argv[])
 {
 #if DEBUG >= 1
-    freopen("test_output.txt", "w", stdout);
+    if (argc > 2)
+        freopen(argv[2], "w", stdout);
+    else
+        freopen("FDLI_log.txt", "w", stdout);
 #endif
+    char *filename;
+    if (argc > 1)
+        filename = argv[1];
+    else
+        filename = "FDLI.txt";
     // 创建分析器
-    Parser parser("test_input.txt");
+    Parser parser(filename);
     // 运行分析器
     parser.run();
     // 绘制图像
